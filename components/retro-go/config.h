b7717d14df53445edace0982cd0ec7bef10bc281
Alejandro J. Cura <alecu@protocultura.net>
2026-06-24 09:38:16 -0300

    Register esp32s3-devkit-c target config

--- components/retro-go/config.h
+++ components/retro-go/config.h
@@ -16,6 +16,8 @@
 #include "targets/esplay-micro/config.h"
 #elif defined(RG_TARGET_ESP32_S3_DEVKIT)
 #include "targets/esp32-s3-devkit/config.h"
+#elif defined(RG_TARGET_ESP32S3_DEVKIT_C)
+#include "targets/esp32s3-devkit-c/config.h"
 #elif defined(RG_TARGET_FRI3D_2024)
 #include "targets/fri3d-2024/config.h"
 #elif defined(RG_TARGET_BYTEBOI_REV1)
